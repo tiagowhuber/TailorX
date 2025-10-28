01fb3228d9fe0c251e4c6d6633529205cc244631
tiagowhuber <thuber2018@udec.cl>
2025-10-27 21:08:25 -0300

    build fix?

--- src/router/index.ts
+++ src/router/index.ts
@@ -53,7 +53,7 @@ const router = createRouter({
 })
 
 // Navigation guard to protect routes that require authentication
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _from, next) => {
   const authStore = useAuthStore()
   
   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
